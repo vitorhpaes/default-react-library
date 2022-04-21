09752d6c6cac2c4337fccd562c2ef0dc1ba53d6c
Vitor Paes <vitor.paes@compasso.com.br>
2022-04-21 19:33:52 -0300

    add eslint packages and other config

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,32 +1,53 @@
 module.exports = {
-  settings: {
-    react: {
-      version: 'detect',
-    },
-  },
   env: {
     browser: true,
     es2021: true,
-    jest: true,
+    node: true,
   },
-  extends: ['plugin:react/recommended', 'standard-with-typescript',
-    'plugin:prettier/recommended'],
+  extends: [
+    "eslint:recommended",
+    "plugin:react/recommended",
+    "plugin:jest/recommended",
+  ],
+  settings: {
+    react: {
+      version: 'detect',
+    }
+  },
   parserOptions: {
-    project: './tsconfig.eslint.json',
     ecmaFeatures: {
       jsx: true,
     },
     ecmaVersion: 12,
-    sourceType: 'module',
+    sourceType: "module",
   },
-  '@typescript-eslint/explicit-function-return-type': 'off',
-  plugins: ['react', '@typescript-eslint', 'prettier'],
+  plugins: ["react", "react-hooks"],
   rules: {
-    '@typescript-eslint/explicit-function-return-type': 'off',
-    'prettier/prettier': 'error',
-    'no-use-before-define': 'off',
-    '@typescript-eslint/no-use-before-define': 'off',
-    'react/prop-types': 'off',
-    'node/handle-callback-err': 'off',
+    semi: [2, "always"],
+    "no-mixed-operators": 0,
+    "prefer-promise-reject-errors": 0,
+    "react/prop-types": 0,
+    "react/no-children-prop": 0,
+    'no-unused-vars': 'warn',
+    "react/display-name": 0,
+    "no-useless-escape": 0,
+    "jest/no-export": 0,
+    "jest/no-test-callback": 0,
+    "promise/param-names": 0,
+    "react/no-string-refs": 0,
+    "standard/no-callback-literal": 0,
+    "react/no-deprecated": 0,
+    "jest/expect-expect": 0,
+    "jest/no-disabled-tests": 0,
+    "jest/no-commented-out-tests": 0,
+    "jest/valid-expect-in-promise": 0,
+    "media-feature-name-no-unknown": 0,
+    "react/jsx-filename-extension": ["warn", {
+      extensions: [".jsx", ".js"]
+    }],
+    "react-hooks/rules-of-hooks": "error",
+    "react-hooks/exhaustive-deps": "warn",
+    "one-var": "off",
   },
-}
+
+};
