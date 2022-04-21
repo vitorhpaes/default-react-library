1023845536de97211a5f7eb23fe23571f28f1b51
Vitor Paes <vitor.paes@compasso.com.br>
2022-04-21 19:22:17 -0300

    alter webpack to rollup compiler

--- public/index.js
+++ public/index.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 
-import TestComponent from '../src/components/test/TestComponent'
+import TestComponent from '../src/components/Button'
 
 const App = () => (
   <>
